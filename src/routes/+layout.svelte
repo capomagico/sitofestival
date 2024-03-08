b6c8cd2337946998d40a35f4071d234d5812ddd8
Capomagico <matty.cap@live.it>
2024-03-08 18:22:13 +0100

    v1.1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,18 @@
-<script lang="ts">
-	import Navbar from '$components/Navbar.svelte';
+<script
+	lang="ts"
+	src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+	crossorigin="anonymous"
+>
 	import '../styles.css';
 </script>
 
-<Navbar />
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+	crossorigin="anonymous"
+/>
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
 
 <slot />
